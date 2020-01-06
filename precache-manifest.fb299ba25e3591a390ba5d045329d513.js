ad0858ad59df03bbf868073e4e1761e36c5ac450
Redouane FARAH <50687682+FARAH-Redouane@users.noreply.github.com>
2020-01-06 00:11:45 +0000

    first

---
+++ precache-manifest.fb299ba25e3591a390ba5d045329d513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c918fb2b41c0f7bc83abb843b060324a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d07828ecff698a98668",
+    "url": "/static/css/main.4c82e1e2.chunk.css"
+  },
+  {
+    "revision": "3bcbbb5f3b62921f0585",
+    "url": "/static/js/2.4f9f83e2.chunk.js"
+  },
+  {
+    "revision": "0d07828ecff698a98668",
+    "url": "/static/js/main.787f774d.chunk.js"
+  },
+  {
+    "revision": "633593413edf98859246",
+    "url": "/static/js/runtime-main.4221dc8a.js"
+  }
+]);
